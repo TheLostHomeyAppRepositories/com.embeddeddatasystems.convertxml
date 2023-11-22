aa4cb7a3af4ba69bddf2d591ec1650684bfd066a
janrikard <102407793+janrikard@users.noreply.github.com>
2023-11-22 16:39:33 +0100

    add support for Dual Addressable Switch DS2406

--- README.txt
+++ README.txt
@@ -3,3 +3,4 @@ Converts EDS XML To Json
 Takes an XML-string in the Embedded Data System format 
 and extract relevant thermometer information and then makes 
 a json-string that homey logic can handle.
+Support reading from Temperature sensor DS18B20 and Dual Addressable Switch DS2406
--- app.js
+++ app.js
@@ -19,27 +19,46 @@ class EDSxml extends Homey.App {
 
       let ROMId = null;
       let temperature = null;
+      let output_A = null;
+      let output_B = null;
       let tempstring = null;
-      let array = [];
-      let jsonstring = "\{\r\n\t\"Temperature\": \{\r\n\t\t\t";
+      let DS18B20array = [];
+      let DS2406array = [];
+      let jsonstring = "";
 
       const { XMLinput } = args;
 
       args.XMLinput.split(/\r?\n/).forEach(line => {
+          
         if (line.includes('<ROMId>')) {
 
           line = line.replace('<ROMId>', '');
           line = line.replace('</ROMId>', '');
           ROMId = line;
-
+          this.log(ROMId);
         }
         if (line.includes('<Temperature Units="Centigrade">')) {
 
           line = line.replace('<Temperature Units="Centigrade">', '');
           line = line.replace('</Temperature>', '');
           temperature = line;
+          this.log(temperature);
         }
 
+        if (line.includes('<InputLevel_A>')) {
+
+          line = line.replace('<InputLevel_A>', '');
+          line = line.replace('</InputLevel_A>', '');
+          output_A = line;
+          this.log(output_A);
+        }
+        if (line.includes('<InputLevel_B>')) {
+
+          line = line.replace('<InputLevel_B>', '');
+          line = line.replace('</InputLevel_B>', '');    
+          output_B = line;
+          this.log(output_B);
+        }
         if (ROMId != null && temperature != null) {
 
           tempstring = "\"";
@@ -49,28 +68,75 @@ class EDSxml extends Homey.App {
           ROMId = null;
           temperature = null;
 
-          array.push(tempstring);
+          DS18B20array.push(tempstring);
         }
+
+        if (ROMId != null && output_A != null && output_B != null) {
+
+          tempstring = "\"";
+          tempstring += ROMId;
+          tempstring += "_A\"\: ";
+          tempstring += output_A;
+          DS2406array.push(tempstring);
+
+          tempstring = "\"";
+          tempstring += ROMId;
+          tempstring += "_B\"\: ";
+          tempstring += output_B;
+          ROMId = null;
+          output_A = null;
+          output_B = null;
+
+          DS2406array.push(tempstring);
+        }
+
+        
 
 
         linenumber++;
       });
 
-      for (let i = 0; i < array.length; i++) {
-        jsonstring += array[i];
-        if (i < (array.length - 1))
+      if (DS18B20array.length > 0)
+      {
+        jsonstring = "\{\r\n\t\"Temperature\": \{\r\n\t\t\t";
+      for (let i = 0; i < DS18B20array.length; i++) {
+        jsonstring += DS18B20array[i];
+        if (i < (DS18B20array.length - 1))
           jsonstring += "\,";
         jsonstring += "\r\n\t\t\t";
 
 
       }
-      array = [];
+    } 
+    if (DS18B20array.length > 0 && DS2406array.length >0)
+
+    {
+      jsonstring += "\r\n\t\t},\r\n\r\n\"DS2406\":\t\t{";
+    }
+    else if (DS2406array.length >0){
+      jsonstring = "\{\r\n\t\"DS2406\": \{\r\n\t\t\t";
+    }
+
+
+    if (DS2406array.length >0){
+      for (let i = 0; i < DS2406array.length; i++) {
+        jsonstring += DS2406array[i];
+        if (i < (DS2406array.length - 1))
+          jsonstring += "\,";
+        jsonstring += "\r\n\t\t\t";
+      }
+
+      }
+
+
+      DS2406array  = [];
+      DS18B20array = [];
 
       jsonstring += "\}\r\n\t\t\r\n\t\}";
       return {
         json_output: jsonstring
       };
-      jsonstring = "\{\r\n\t\"Temperature\": \{\r\n\t\t\t";
+      
     })
 
 
